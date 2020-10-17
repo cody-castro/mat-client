42d77873b438b447a0a08f39dec496adfe725c99
CodyC <codyicastro@gmail.com>
2020-10-16 21:30:47 -0400

    map with reactmapgl

--- src/Homepage.js
+++ src/Homepage.js
@@ -8,15 +8,18 @@ import ReviewCard from './ReviewCard'
 
 function Homepage(){
 
-    const requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      };
-      
-      fetch("https://data.cityofnewyork.us/resource/he7q-3hwy.json", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+//     const requestOptions = {
+//         method: 'GET',
+//         redirect: 'follow'
+//       };
+//       function getData(){
+//       fetch("https://cors-anywhere.herokuapp.com/https://cody-castro.github.io/Data/Subway%20Entrances%20(3).geojson", requestOptions)
+//         .then(response => response.json())
+//         .then(result => console.log(result))
+//         .catch(error => console.log('error', error));
+//       }
+
+// getData()
 
     return(    
         <div className="homepage">
--- src/Map.js
+++ src/Map.js
@@ -3,118 +3,142 @@ import mapboxgl from "mapbox-gl";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import { NavLink } from 'react-router-dom';
+import ReactMapGL, {Layer} from 'react-map-gl';
 
 
 
 
-mapboxgl.accessToken = 'slack'
 
 
 class Map extends React.Component {
 
   state = {
-		lat: 40.7,
-		lng: -73.96,
-		zoom: 11,
-		pitch: 1.5, // pitch in degrees
-    bearing: 28.81, // bearing in degrees
-    subwayLine: []
-  };
-  
-  
-  // getSubwayLines = ()=>{
-  //   var requestOptions = {
-  //     method: 'GET',
-  //     redirect: 'follow'
-  //   };
-    
-  //   fetch("https://data.cityofnewyork.us/resource/s7zz-qmyz.json", requestOptions)
-  //   .then(response => response.json())
-  //   .then(result => console.log(result[0]["the_geom"]["coordinates"]))
-  //   .catch(error => console.log('error', error))
-  // }
-  
-  
-componentDidMount() {
-    const map = new mapboxgl.Map({
-      container: this.mapContainer, // HTML container id
-      style: 'mapbox://styles/nycody/ckg70msbu0bsq19ntsnzxhz22/draft', // style URL + THIS IS A DRAFT NEEDS TO BE UPDATED!
-      center: [this.state.lng, this.state.lat],
-      zoom: this.state.zoom, 
-      maxZoom: 18,
-      minZoom: 10,
-      pitch: this.state.pitch, 
-      bearing: this.state.bearing
-    })
-    
-    //Search Bar Begin
-    const geocoder = new MapboxGeocoder({
+    viewport: {
+      width: "100vw",
+      height: "100vh",
+      lat: 40.7831,
+      lng: 73.9712,
+      zoom: 15,
+      pitch: 1.5, // pitch in degrees
+      bearing: 28.81, // bearing in degrees
 
-      accessToken: mapboxgl.accessToken,
-      marker: {
-      color: 'green'
-      },
-      mapboxgl: mapboxgl
-      });
-      map.addControl(geocoder);
-//Search Bar End
-    
-
-map.on('click', function(e) {
-  var features = map.queryRenderedFeatures(e.point, {
-    layers: ['subway-entrances'] 
-  });
-
-  if (!features.length) {
-    return;
-  }
-
-  const feature = features[0];
-
-  const popup = new mapboxgl.Popup({ offset: [0, -15] })
-    .setLngLat(feature.geometry.coordinates)
-    .setHTML(
-      writeReview(feature) + `<a href="http://localhost:3001/review/"><button onClick="">Review It!</button></a>`
-      )
-    .addTo(map)
-});
-
-
-
-{/* <ReviewPage id={feature.properties.id}/> */}
-
- function writeReview(feature)  {
- return ('<strong>' 
- + feature.properties.name + 
- '</strong> <p> Lines: ' 
- + feature.properties.line + 
- '</p>' 
- )
-}
-
-    //The ending bracket for compDidMount  
-  }
-  
-
-
-  
-  render() {
-
-
-    return (
-      <>
-      <div>
-        <div ref={el => this.mapContainer = el} className="mapContainer" />
-      </div>
-    </>
-      )
+      // maxZoom: 18,
+      // minZoom: 10
+    },
+    stationLayer:{
+      id: 'ckg9gs84802ub22qow819e45r',
+      type: 'circle',
+      source: 'mapbox',
+      'source-layer': 'Subway_Entrances_',
     }
 
+  };
+
+  
+// fetch("http://localhost:3000/locations")
+
+
+  render() {
+        return (
+<>
+          <ReactMapGL {...this.state.viewport} onViewportChange={viewport => this.setState({viewport})} mapStyle="mapbox://styles/nycody/ckgcysc6u4htx19p7l9z0g4a1/draft" mapboxApiAccessToken="slack"/>
+            <Layer {...this.state.stationLayer}  />
+          <ReactMapGL />
+</>
+        )
+    }
+
+  }
+
+export default Map; 
+
+
+//Old Map stuff
+
+// componentDidMount() {
+  //     const map = new mapboxgl.Map({
+    //       container: this.mapContainer, // HTML container id
+    //       style: 'mapbox://styles/nycody/ckg70msbu0bsq19ntsnzxhz22/draft', // style URL + THIS IS A DRAFT NEEDS TO BE UPDATED!
+    //       center: [this.state.lng, this.state.lat],
+    //       zoom: this.state.zoom, 
+    //       maxZoom: 18,
+    //       minZoom: 10,
+    //       pitch: this.state.pitch, 
+    //       bearing: this.state.bearing
+    //     })
+    
+    // getSubwayLines = ()=>{
+    //   var requestOptions = {
+    //     method: 'GET',
+    //     redirect: 'follow'
+    //   };
+      
+    //   fetch("https://data.cityofnewyork.us/resource/s7zz-qmyz.json", requestOptions)
+    //   .then(response => response.json())
+    //   .then(result => console.log(result[0]["the_geom"]["coordinates"]))
+    //   .catch(error => console.log('error', error))
+    // }
+
+
+    //     //Search Bar Begin
+    //     const geocoder = new MapboxGeocoder({
+      
+//       accessToken: mapboxgl.accessToken,
+//       marker: {
+//       color: 'green'
+//       },
+//       mapboxgl: mapboxgl
+//       });
+//       map.addControl(geocoder);
+// //Search Bar End
+    
+
+// map.on('click', function(e) {
+//   var features = map.queryRenderedFeatures(e.point, {
+//     layers: ['subway-entrances'] 
+//   });
+
+//   if (!features.length) {
+//     return;
+//   }
+
+//   const feature = features[0];
+
+//   const popup = new mapboxgl.Popup({ offset: [0, -15] })
+//     .setLngLat(feature.geometry.coordinates)
+//     .setHTML(
+//       writeReview(feature) + `<a href="http://localhost:3001/review/"><button onClick="">Review It!</button></a>`
+//       )
+//     .addTo(map)
+// });
 
 
 
+// {/* <ReviewPage id={feature.properties.id}/> */}
 
-}
+//  function writeReview(feature)  {
+//  return ('<strong>' 
+//  + feature.properties.name + 
+//  '</strong> <p> Lines: ' 
+//  + feature.properties.line + 
+//  '</p>' 
+//  )
+// }
 
-export default Map 
+//     //The ending bracket for compDidMount  
+//   }
+  
 
+
+  
+//   render() {
+
+
+//     return (
+//       <>
+//       <div>
+//         <div ref={el => this.mapContainer = el} className="mapContainer" />
+//       </div>
+//     </>
+//       )
+//     }
