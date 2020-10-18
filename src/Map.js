644b89ab92f6a092033676ca3e34519078887001
CodyC <codyicastro@gmail.com>
2020-10-18 15:28:30 -0400

    incremntal commit

--- src/Map.js
+++ src/Map.js
@@ -1,52 +1,83 @@
-import React from 'react';
-import mapboxgl from "mapbox-gl";
-import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import React, {useState, useRef, Component} from 'react';
+// import mapboxgl from "mapbox-gl";
+// import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
-import { NavLink } from 'react-router-dom';
-import ReactMapGL, {Layer} from 'react-map-gl';
+// import { NavLink } from 'react-router-dom';
+import ReactMapGL, {Layer, Source, Popup, Marker} from 'react-map-gl';
+import useSwr from "swr";
+
+const fetcher = (...args) => fetch(...args).then(resp => resp.json()).then(data => console.log(data))
 
 
+function Map() {
 
-
-
-
-class Map extends React.Component {
-
-  state = {
-    viewport: {
+  const [viewport, setViewport, setLayer] = useState( {
+    latitude: 40.709244800046726,
+    longitude: -73.84787727630345,
       width: "100vw",
       height: "100vh",
-      lat: 40.7831,
-      lng: 73.9712,
-      zoom: 15,
+      zoom: 9.503816871606016,
       pitch: 1.5, // pitch in degrees
       bearing: 28.81, // bearing in degrees
+      // layerData: {}
+    })
 
-      // maxZoom: 18,
-      // minZoom: 10
-    },
-    stationLayer:{
-      id: 'ckg9gs84802ub22qow819e45r',
-      type: 'circle',
-      source: 'mapbox',
-      'source-layer': 'Subway_Entrances_',
-    }
-
-  };
-
-  
-// fetch("http://localhost:3000/locations")
+    const mapRef = useRef();
 
 
-  render() {
-        return (
-<>
-          <ReactMapGL {...this.state.viewport} onViewportChange={viewport => this.setState({viewport})} mapStyle="mapbox://styles/nycody/ckgcysc6u4htx19p7l9z0g4a1/draft" mapboxApiAccessToken="slack"/>
-            <Layer {...this.state.stationLayer}  />
-          <ReactMapGL />
+    const stationDataUrl = "http://localhost:3000/locations"
+    const {data, error} = useSwr(stationDataUrl, fetcher)
+
+    const stations = data && !error ? data : [];
+
+
+  //   stationLayer:{
+  //     id: 'ckg9gs84802ub22qow819e45r',
+  //     type: 'point',
+  //     source: {},
+  //     width: "100vw",
+  //     height: "100vh",
+  //   }
+  // };
+
+
+// function getData(){
+//   fetch("http://localhost:3000/locations").then(resp => resp.json()).then(data => { 
+//    return newLayerData => {setLayer( {...newLayerData} )}
+//   })
+// }  
+
+
+
+
+
+return (
+  <>
+  {/* {console.log(stations)} */}
+          <ReactMapGL 
+          {...viewport} 
+          onViewportChange={newViewport => {setViewport({ ...newViewport })}} 
+          mapStyle="mapbox://styles/nycody/ckgcysc6u4htx19p7l9z0g4a1/draft"  
+          mapboxApiAccessToken="pk.eyJ1Ijoibnljb2R5IiwiYSI6ImNrZmcxZWFuejAzNWEydHIyMmw5eGIxaWwifQ.7p4RHp9R5RXRDe6YyktAnQ"
+          maxZoom={20}
+          >
+          {/*  <Source 
+            type="GeoJSON">
+              <Layer 
+              {...this.state.stationLayer}
+              />
+
+            </Source> */}
+            {stations.map(station => (
+              <Marker className="station-popup" latitude={parseFloat(station.coordinates.x)} longitude={parseFloat(station.coordinates.y)} key={station.id}>
+                <button>
+                  <img src="https://cdn2.iconfinder.com/data/icons/train-circle-filled/100/023-_Train_-_Rail_-_Railroad_-_Railway_-_Tracks_-_Transport-512.png" alt="poop"/>
+                </button>
+              </Marker>
+            ))}
+          </ReactMapGL>
 </>
         )
-    }
 
   }
 
