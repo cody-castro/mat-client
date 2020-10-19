a6c526dac0a8e2874a1b2bdf0a2194415ff6f208
CodyC <codyicastro@gmail.com>
2020-10-18 22:14:24 -0400

    stations rendered, popup & markers

--- src/Map.js
+++ src/Map.js
@@ -1,85 +1,142 @@
-import React, {useState, useRef, Component} from 'react';
+import React, {useState, useRef, Component, useEffect} from 'react';
 // import mapboxgl from "mapbox-gl";
 // import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 // import { NavLink } from 'react-router-dom';
 import ReactMapGL, {Layer, Source, Popup, Marker} from 'react-map-gl';
-import useSwr from "swr";
+// import useSwr from "swr";
+import useSupercluster from "use-supercluster";
 
-const fetcher = (...args) => fetch(...args).then(resp => resp.json()).then(data => console.log(data))
+
 
 
 function Map() {
-
-  const [viewport, setViewport, setLayer] = useState( {
+  
+  const [viewport, setViewport] = useState( {
     latitude: 40.709244800046726,
     longitude: -73.84787727630345,
-      width: "100vw",
-      height: "100vh",
-      zoom: 9.503816871606016,
-      pitch: 1.5, // pitch in degrees
-      bearing: 28.81, // bearing in degrees
-      // layerData: {}
-    })
+    width: "100vw",
+    height: "100vh",
+    zoom: 9.503816871606016,
+    pitch: 1.5, // pitch in degrees
+    bearing: 28.81, // bearing in degrees
+  })
+  
+  
+  const mapRef = useRef();
+  
+  const [stations, setStations] = useState([])
+  
+  const [popups, setShowPopup] = useState(null)
+  
+  const stationDataUrl = "http://localhost:3000/locations/"
+  
 
-    const mapRef = useRef();
-
-
-    const stationDataUrl = "http://localhost:3000/locations"
-    const {data, error} = useSwr(stationDataUrl, fetcher)
-
-    const stations = data && !error ? data : [];
-
-
-  //   stationLayer:{
-  //     id: 'ckg9gs84802ub22qow819e45r',
-  //     type: 'point',
-  //     source: {},
-  //     width: "100vw",
-  //     height: "100vh",
-  //   }
-  // };
+    // const bounds = mapRef.current ? mapRef.current.getMap().getBounds().getArray().flat() : null;
 
 
-// function getData(){
-//   fetch("http://localhost:3000/locations").then(resp => resp.json()).then(data => { 
-//    return newLayerData => {setLayer( {...newLayerData} )}
-//   })
-// }  
+  function getData() {
+      fetch(stationDataUrl).then(resp => resp.json()).then(data => setStations(data) )
+    }
 
+    const points = stations.map(station => ({
+    type: "Feature",
+    properties: { cluster: false, stationId: station.id, },
+    geometry: {
+      type: "Point",
+      coordinates: [
+        parseFloat(station.coordinates.x),
+        parseFloat(station.coordinates.y)]}
+      }))
+      
+    // const { clusters, supercluster } = useSupercluster({
+    //     points,
+    //     bounds,
+    //     zoom: viewport.zoom,
+    //     options: { radius: 75, maxZoom: 20 }
+    //   });
+      
+      
+      
+      useEffect( ()=>{
+        getData()
+      }, [])
+      
+      function closePopup() {
+        setShowPopup(null)
+      };
+      
 
-
-
-
-return (
-  <>
-  {/* {console.log(stations)} */}
-          <ReactMapGL 
-          {...viewport} 
-          onViewportChange={newViewport => {setViewport({ ...newViewport })}} 
-          mapStyle="mapbox://styles/nycody/ckgcysc6u4htx19p7l9z0g4a1/draft"  
-          mapboxApiAccessToken="pk.eyJ1Ijoibnljb2R5IiwiYSI6ImNrZmcxZWFuejAzNWEydHIyMmw5eGIxaWwifQ.7p4RHp9R5RXRDe6YyktAnQ"
-          maxZoom={20}
-          >
-          {/*  <Source 
-            type="GeoJSON">
-              <Layer 
-              {...this.state.stationLayer}
-              />
-
-            </Source> */}
-            {stations.map(station => (
-              <Marker className="station-popup" latitude={parseFloat(station.coordinates.x)} longitude={parseFloat(station.coordinates.y)} key={station.id}>
-                <button>
-                  <img src="https://cdn2.iconfinder.com/data/icons/train-circle-filled/100/023-_Train_-_Rail_-_Railroad_-_Railway_-_Tracks_-_Transport-512.png" alt="poop"/>
-                </button>
-              </Marker>
-            ))}
-          </ReactMapGL>
-</>
-        )
-
+      function popupHandler(e){
+        setShowPopup(e)
+    console.log(e)
   }
+  
+  function writeReview(e){
+    e.preventDefault(e)
+    console.log("yeeee")
+  }
+  
+  function popupStuff(){
+    return(
+      <ul>
+                        <li>
+                          {popups.name}
+                        </li>
+                        <li>
+                          {popups.line}
+                        </li>
+                        <li>
+                          <div id="review" onClick={(e) =>{
+                            console.log(e.targetValue)}}>Review This Station
+                            </div>
+                        </li>
+                      </ul>
+    )
+  }
+
+  
+  
+ return (
+    <>
+            <ReactMapGL 
+            {...viewport} 
+            onViewportChange={newViewport => {setViewport({ ...newViewport })}} 
+            mapStyle="mapbox://styles/nycody/ckgcysc6u4htx19p7l9z0g4a1/draft"  
+            mapboxApiAccessToken="pk.eyJ1Ijoibnljb2R5IiwiYSI6ImNrZmcxZWFuejAzNWEydHIyMmw5eGIxaWwifQ.7p4RHp9R5RXRDe6YyktAnQ"
+            maxZoom={20}
+            ref={mapRef}
+            >
+
+              {popups !== null ? (
+                    <Popup
+                      zoom="20"
+                      latitude={parseFloat(popups.coordinates.y)}
+                      longitude={parseFloat(popups.coordinates.x)}
+                      onClose={()=>{closePopup()}}
+                      id="popup"
+                    >
+                      {popupStuff()}
+                    </Popup>
+                    ) : null}
+            
+
+              {stations.map(station => (
+                <Marker id={station.id} latitude={station.coordinates.y} longitude={station.coordinates.x} cluster={true}
+                clusterMaxZoom={14}
+                clusterRadius={2}
+                ref={mapRef}>
+
+                  <img height="5" width="relative" src="http://maps.google.com/mapfiles/ms/micons/rail.png" alt="station icon" onClick= {e => { popupHandler(station)}}></img>
+                  
+                </Marker>
+
+              ))}
+            </ReactMapGL>
+          </>
+          )
+
+}
 
 export default Map; 
 
